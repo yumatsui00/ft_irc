f1b262f8434167fdaef5bdc5614e3f7dcca22733
kazuhiro <okpthen@gmail.com>
2024-07-14 00:30:09 +0900

    a

---
+++ inc/sebserv.hpp
@@ -0,0 +1,21 @@
+#ifndef WEBSERV_HPP
+# define WEBSERV_HPP
+
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <iostream>
+#include <poll.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/epoll.h>
+#include <sys/select.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+
+#endif
