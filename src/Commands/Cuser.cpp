f5aa44f96f87eed051bb067aba8aaaab254aec51
kazuhiro <okpthen@gmail.com>
2024-07-25 07:14:55 +0900

    ログイン関係直した

--- src/Commands/Cuser.cpp
+++ src/Commands/Cuser.cpp
@@ -1,13 +1,13 @@
 #include "Command.hpp"
 
 int	Command::user( Server &server ) {
-	// if (!this->_user->getpassok()) {
-	// 	std::cout << "kokoka??????" << std::endl;
-	// 	close(_user->getFd());
-	// 	return (0);
-	// }
+	if (!this->_user->getpassok()) {
+		// std::cout << "kokoka??????" << std::endl;
+		close(_user->getFd());
+		return (0);
+	}
 	std::cout << _user->isRegistered() << std::endl;
-	if (_user->isRegistered())
+	if (_user->isRegistered())//ここバグってるよ
 		return (0); //すでにloginしてある
 	if (_divCmd.size() != 5)
 		return (461);
