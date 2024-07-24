90408789507b55253786c431207faefea26446b7
Kazuhiro Okada <kazokada@c2r7s10.42tokyo.jp>
2024-07-24 14:33:42 +0900

    a

--- src/Commands/Privmsg.cpp
+++ src/Commands/Privmsg.cpp
@@ -10,24 +10,10 @@ int	Command::privmsg( Server &server ) {
 	if (channel) {
 		std::map<User*, bool> users = channel->getUsers();
 		for (std::map<User*, bool>::iterator it = users.begin(); it != users.end(); it++) {
-			server.ft_send(it->first->getFd(), msg);
+			if (it->first != _user)
+				server.ft_send(it->first->getFd(), msg);
 		}
-		if (_divCmd[2] == "じゃんけんグー") {
-			msg = ":じゃんけんbot君 PRIVMSG " + _divCmd[1] + " :じゃんけんパー！✋" + "\n";
-			for (std::map<User*, bool>::iterator it = users.begin(); it != users.end(); it++) {
-				server.ft_send(it->first->getFd(), msg);
-			}
-		} else if (_divCmd[2] == "じゃんけんチョキ") {
-			msg = ":じゃんけんbot君 PRIVMSG " + _divCmd[1] + " :じゃんけんグー！👊" + "\n";
-			for (std::map<User*, bool>::iterator it = users.begin(); it != users.end(); it++) {
-				server.ft_send(it->first->getFd(), msg);
-			}
-		} else if (_divCmd[2] == "じゃんけんパー") {
-			msg = ":じゃんけんbot君 PRIVMSG " + _divCmd[1] + " :じゃんけんチョキ！✌️✋" + "\n";
-			for (std::map<User*, bool>::iterator it = users.begin(); it != users.end(); it++) {
-				server.ft_send(it->first->getFd(), msg);
-			}
-		} 
+		janken( server, users );
 	} else {
 		User* target = server.nick2User(_divCmd[1]);
 		if (target == NULL)
@@ -35,4 +21,38 @@ int	Command::privmsg( Server &server ) {
 		server.ft_send(target->getFd(), msg);
 	}
 	return (0);
+}
+
+void	Command::janken( Server &server, std::map<User*, bool> users ) {
+	std::string msg;
+
+	if (_divCmd[2] == "じゃんけんグー") {
+		msg = ":じゃんけんbot君 PRIVMSG " + _divCmd[1] + " :じゃんけんパー！✋" + "\n";
+		for (std::map<User*, bool>::iterator it = users.begin(); it != users.end(); it++) {
+			server.ft_send(it->first->getFd(), msg);
+		}
+	} else if (_divCmd[2] == "じゃんけんチョキ") {
+		msg = ":じゃんけんbot君 PRIVMSG " + _divCmd[1] + " :じゃんけんグー！👊" + "\n";
+		for (std::map<User*, bool>::iterator it = users.begin(); it != users.end(); it++) {
+			server.ft_send(it->first->getFd(), msg);
+		}
+	} else if (_divCmd[2] == "じゃんけんパー") {
+		msg = ":じゃんけんbot君 PRIVMSG " + _divCmd[1] + " :じゃんけんチョキ！✌️" + "\n";
+		for (std::map<User*, bool>::iterator it = users.begin(); it != users.end(); it++) {
+			server.ft_send(it->first->getFd(), msg);
+		}
+	} else if (_divCmd[2].find("じゃんけん") != std::string::npos){
+		std::srand(std::time(0));
+    	int result = std::rand() % 3;
+		if (result == 0){
+			msg = ":じゃんけんbot君 PRIVMSG " + _divCmd[1] + " :じゃんけんチョキ！✌️" + "\n";
+		} else if (result == 1) {
+			msg = ":じゃんけんbot君 PRIVMSG " + _divCmd[1] + " :じゃんけんグー！👊" + "\n";
+		} else {
+			msg = ":じゃんけんbot君 PRIVMSG " + _divCmd[1] + " :じゃんけんパー！✋" + "\n";
+		}
+		for (std::map<User*, bool>::iterator it = users.begin(); it != users.end(); it++) {
+			server.ft_send(it->first->getFd(), msg);
+		}
+	}
 }
