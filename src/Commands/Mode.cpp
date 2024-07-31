526a55971d411bb7d8990fe7f619fc61b51b3e8e
Yuma Matsui <yumatsui@c2r6s10.42tokyo.jp>
2024-07-31 17:33:14 +0900

    ifx

--- src/Commands/Mode.cpp
+++ src/Commands/Mode.cpp
@@ -4,8 +4,11 @@ int	Command::mode( Server &server ) {
 	if (!(2 <= _divCmd.size() && _divCmd.size() <= 4))
 		return (461);
 	Channel *channel = server.findChannel(_divCmd[1]);
-	if (channel == NULL)
+	if (channel == NULL) {
+		if (_divCmd[1] == _user->getNickName())
+			return (0);
 		return (403);
+	}
 	if (!channel->isExist(_user))
 		return (442);
 	if (_divCmd.size() == 2)
